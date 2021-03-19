1d4124576095ef1de9fa5b64599573dac5cfa8ff
Luisjwhitaker <luisjwhitaker@gmail.com>
2021-03-19 09:31:44 +0800

    working roll_die function with no GUI.

---
+++ roll_die.py
@@ -0,0 +1,14 @@
+import random
+
+def roll_die(die_sides,rolls=1):
+    '''
+    Takes number of sides of the die as the first argument, and number of rolls as the second optional argument,
+
+    example:
+        roll_die(6) will roll a six sided die once.
+        roll_die(20,3) will roll a twenty sided die three times.
+    '''
+    answer = []
+    for i in range(rolls):
+        answer += [random.choice(range(1,die_sides+1))]
+    return answer
